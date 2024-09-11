4deb5419e4f354c2ee8f1ac3f3995c32e1e34f09
Felipe25687 <felipemofernandes@gmail.com>
2024-09-11 12:15:22 -0300

    atualizacao reserva

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -16,15 +16,18 @@ namespace DesafioProjetoHospedagem.Models
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+            int qtd  =  Suite.Capacidade; 
+            int cadastro = hospedes.Capacity;           
+
+            if (qtd >= cadastro)
             {
-                Hospedes = hospedes;
+                Hospedes = hospedes;                
+
             }
             else
             {
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+                throw new System.ArgumentException("Capacidade insuficiente!");
             }
         }
 
@@ -36,22 +39,25 @@ namespace DesafioProjetoHospedagem.Models
         public int ObterQuantidadeHospedes()
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            int qtd  =  Hospedes.Capacity; 
+            return qtd;
         }
 
         public decimal CalcularValorDiaria()
         {
             // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
+            // Cálculo: DiasReservados X Suite.ValorDiaria            
             decimal valor = 0;
+            valor = DiasReservados * Suite.ValorDiaria;
+            return valor;
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+            
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+
+                valor = DiasReservados * (decimal)Suite.ValorDiaria - (0.1m * DiasReservados * (decimal)Suite.ValorDiaria);
+
             }
 
             return valor;
